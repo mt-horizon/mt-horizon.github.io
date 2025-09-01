6a2ae3cf519f35a1625072ad75173970180ea6bf
颜姬 <36791327+yanjisheng@users.noreply.github.com>
2025-09-01 08:49:06 +0800

    Add files via upload

---
+++ common.js
@@ -0,0 +1,65 @@
+function resize(){
+    let width = document.body.clientWidth;
+    let products = document.getElementsByClassName("product");
+    if(products.length <= 0){
+        return -1;
+    }
+    if(width < 240){
+        for(let i=0; i<products.length; i++){
+            products[i].style.display="block";
+            products[i].style.width=width-40+"px";
+            products[i].style.margin="10px auto";
+            products[i].lastChild.lastChild.style.width=width-40+"px";
+            products[i].lastChild.lastChild.style.paddingLeft=0;
+        }
+    }else if(width < 668){
+        for(let i=0; i<products.length; i++){
+            products[i].style.display="block";
+            products[i].style.width=width-40+"px";
+            products[i].style.margin="10px auto";
+            products[i].lastChild.lastChild.style.width=width-155+"px";
+            products[i].lastChild.lastChild.style.paddingLeft="10px";
+        }
+    }else{
+        for(let i=0; i<products.length; i++){
+            products[i].style.display="inline-block";
+            products[i].style.width=width/2-42+"px";
+            products[i].style.margin="10px 10px";
+            products[i].lastChild.lastChild.style.width=width/2-157+"px";
+            products[i].lastChild.lastChild.style.paddingLeft="10px";
+        }
+    }
+    return products.length;
+}
+var r = resize();
+if(r > 0){
+    window.onresize = resize;
+}
+function language(){
+    window.location.href = "English.html";
+}
+var keywords = ["测力台", "电荷放大器", "制动测试", "温度", "数据采集", "三向力"];
+var index = Math.floor(Math.random() * keywords.length);
+function updateKeyword(){
+    if(index >= keywords.length){
+        index = 0;
+    }
+    document.getElementById("keyword").value = keywords[index];
+    index += 1;
+}
+var u = setInterval(updateKeyword, 10000);
+document.getElementById("keyword").onfocus = function(){
+    clearInterval(u);
+}
+function search(){
+    let keyword = document.getElementById("keyword").value;
+    if(keyword.length > 0){
+        window.open("https://cn.bing.com/search?q="+keyword+"+site%3amthorizon.com");
+    }
+}
+document.getElementById("search").onclick = search;
+document.getElementById("keyword").onkeydown = function(e){
+    if(e.key === "Enter"){
+        search();
+    }
+}
